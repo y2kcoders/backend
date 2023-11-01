9b54e9535fb7bc0893020b95fc68ea93bffee9b2
Mati Danish <matiullah.danish5@gmail.com>
2023-11-01 12:56:37 -0700

    hi :)

--- src/main/java/com/nighthawk/spring_portfolio/mvc/chatboard/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nighthawk.spring_portfolio.mvc.chatboard;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-     List<Comment>findCommentsByPost(Post post);
-}
