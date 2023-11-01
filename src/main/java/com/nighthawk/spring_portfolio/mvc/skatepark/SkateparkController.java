2d953aae72fbd30ea3c4408c14dd0634f4ae868a
F1nnC <captainsurfervideos@gmail.com>
2023-11-01 14:28:31 -0700

    ok fixed

--- src/main/java/com/nighthawk/spring_portfolio/mvc/skatepark/SkateparkController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/skatepark/SkateparkController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/skatepark")
@@ -25,19 +24,6 @@ public class SkateparkController {
         return new ResponseEntity<>(savedSkatepark, HttpStatus.CREATED);
     }
 
-    // @PostMapping("/like/{name}")
-    // public ResponseEntity<Skatepark> updateLikes(@PathVariable String name, @RequestBody int likeValue) {
-    //     List<Skatepark> skateparks = repository.findBySkateparkName(name);
-    //     if (!skateparks.isEmpty()) {
-    //         Skatepark skatepark = skateparks.get(0); // Assuming you want to work with the first matching skatepark
-    //         int currentLikes = skatepark.getTotalLikes();
-    //         skatepark.setTotalLikes(currentLikes + likeValue);
-    //         // You can also update the author who liked the skatepark
-    //         repository.save(skatepark);
-    //         return new ResponseEntity<>(skatepark, HttpStatus.OK);
-    //     }
-    //     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    // }
 
     @PostMapping("/like/{name}")  // Change the path variable to 'name'
     public ResponseEntity<Skatepark> updateLikes(@PathVariable String name) {
